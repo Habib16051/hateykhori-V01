027e5fd5564d1a5a439cc9dfef34a9bb2ccbf9ee
Habib16051 <habibmhr143@gmail.com>
2024-09-07 15:15:01 +0600

    cart counting is fixed

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from accounts.models import Account
+from cart.models import Cart, CartItem
+from cart.views import _cart_id
 from .forms import RegistrationForm
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
@@ -62,6 +64,18 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
         
         if user is not None:
+            try:
+              cart = Cart.objects.get(cart_id=_cart_id(request))
+              is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+              if is_cart_item_exists:
+                  cart_item = CartItem.objects.filter(cart=cart)
+
+                  for item in cart_item:
+                      item.user = user
+                      item.save()
+
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'Login successful')
             
